2d1c81b27d481a600c3020ea6ee56964fbada660
Alyn.Fosado <210764@utxicotepec.edu.mx>
2024-06-13 15:19:09 -0600

    examen

---
+++ Frontend/Sistema-Hospital/src/components/Pie-Pagina.vue
@@ -0,0 +1,3 @@
+<a>PIE PAGINA
+        
+</a>
---
+++ Frontend/Sistema-Hospital/src/router/index.js
@@ -0,0 +1,116 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import LoginView from '@/components/Login.vue'
+import RegisterView from '@/components/Register.vue'
+import DashboardView from '@/components/Dashboard.vue'
+import UserView from '@/components/User.vue'
+import PersonView from '@/components/Person.vue'
+import RequestView from '@/components/RequestTransplant.vue'
+import TableRView from '@/components/TableRequest.vue'
+import OrgansView from '@/components/Organs.vue'
+import piepagina from '@/components/Pie-Pagina.vue'
+
+
+
+
+
+
+import OrganDetails from '@/components/Organ_Details.vue'
+import OrganForm from '@/components/OrganForm.vue'
+import PiePagina from '@/components/Pie-Pagina.vue'
+
+
+
+
+
+
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashboardView,
+      children:[{
+        path: '/person',
+        name: 'person',
+        component: PersonView
+      },
+    {
+      path: '/requestTansplant',
+      name: 'requestTransplant',
+      component: RequestView
+    },
+    {
+      path: '/TableTransplante',
+      name: 'TableTransplante',
+      component: TableRView
+    },
+    {
+      path: '/organos',
+      name: 'organos',
+      component: OrgansView
+    }
+
+
+
+
+
+
+
+
+
+
+    ,{
+      path: '/organdetails',
+      name: 'organdetails',
+      component: OrganDetails
+
+    },
+    {
+      path: '/organform',
+      name: 'organform',
+      component: OrganForm
+    },
+    {
+      path: '/piepagina',
+      name: 'piepagina',
+      component: PiePagina
+    }
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+  ]
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
+})
+
+export default router
